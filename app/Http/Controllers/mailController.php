3d8420a56702e576ffee9ca4ee7140c4a010c53f
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2021-08-17 20:59:29 -0700

    ok

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
 
 class mailController extends Controller
 {
    public static function sendMail($reciever,$subject,$data,$view)
    {
-       Mail::send($view, ['data' => $data], function ($m) use ($subject,$reciever) {
+       Session::put('subject',$subject);
+       Session::put('reciever',$reciever);
+
+       Mail::send($view, ['data' => $data], function ($m)  {
            $m->from(Config::get('myconfig.mail.from'),Config::get('myconfig.mail.company_name'))
-               ->subject($subject)
-               ->to($reciever,"ok");
+               ->subject($_SESSION['subject'])
+               ->to($_SESSION['reciever'],"ok");
 
        });
    }
